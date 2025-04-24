e4ad80d4aa9c294e5144c41b6efa776265070d29
Kevin wasonga <kevinwasonga415@outlook.com>
2025-04-25 01:56:37 +0300

    try1

--- server/server.js
+++ server/server.js
@@ -17,13 +17,13 @@ app.use(cors());
 
 app.use("/app", require("./routes/api"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "../client/dist")));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+});
+// }
 
 const port = process.env.PORT || 3000;
 
